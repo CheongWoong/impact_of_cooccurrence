bb43d2f1e0e58763f53d6216a290e9a3f70396fb
CheongWoong <cw.kang@kaist.ac.kr>
2024-03-05 10:31:42 +0900

    update

--- scripts/mitigation/debiasing/debiasing_with_undersampling.sh
+++ scripts/mitigation/debiasing/debiasing_with_undersampling.sh
@@ -1,3 +1,4 @@
-dataset_name="LAMA_TREx"
+pretraining_dataset_name=$1
+dataset_name=$2
 
-python -m src.mitigation.debiasing.debiasing_with_undersampling --dataset_name $dataset_name
+python -m src.mitigation.debiasing.debiasing_with_undersampling --pretraining_dataset_name $pretraining_dataset_name --dataset_name $dataset_name
